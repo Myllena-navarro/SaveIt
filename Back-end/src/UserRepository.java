71d82a254b527bf5bddc6790d6962b16845f68b3
Débora Buriti <deboraburiti@gmail.com>
2025-04-28 00:50:33 -0300

    Remover antigo projeto back-end

--- Back-end/src/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.saveit.repository;
-
-import com.saveit.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
-
