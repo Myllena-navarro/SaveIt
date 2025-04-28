cc367e0cb1c6cb77591e560da6f01e59d9888dde
Débora Buriti <deboraburiti@gmail.com>
2025-04-28 09:23:43 -0300

    Adicionar novo escorpo do projeto Spring

---
+++ Back-end/back-end/saveit/src/main/java/br/com/saveit/SaveitApplication.java
@@ -0,0 +1,13 @@
+package br.com.saveit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveitApplication.class, args);
+	}
+
+}
