a1c16d9d8d42abcf1b4b42050c345671940490d2
Myllena Navarro <128384896+Myllena-navarro@users.noreply.github.com>
2025-04-28 18:00:29 -0300

    Add files via upload

---
+++ app-saveit/src/test/java/com/saveit/app_saveit/AppSaveitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveit.app_saveit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSaveitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
