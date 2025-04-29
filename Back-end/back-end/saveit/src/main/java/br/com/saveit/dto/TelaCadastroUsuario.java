c00c8dfde2296c117582ea9fc8d370979e4ade05
Débora Buriti <deboraburiti@gmail.com>
2025-04-29 10:12:52 -0300

    Adicionar setters na tela de login e cadastro

--- Back-end/back-end/saveit/src/main/java/br/com/saveit/dto/TelaCadastroUsuario.java
+++ Back-end/back-end/saveit/src/main/java/br/com/saveit/dto/TelaCadastroUsuario.java
@@ -14,20 +14,39 @@ public class TelaCadastroUsuario {
         return nome;
     }
 
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
     public LocalDate getDataNascimento() {
         return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
     }
 
     public String getCpf() {
         return cpf;
     }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getSenha() {
         return senha;
     }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
