094bc17ca1048e035e3a19af15821a1a0c644c8e
Débora Buriti <deboraburiti@gmail.com>
2025-04-29 02:28:23 -0300

    Refatorar Dto e controllers

--- Back-end/back-end/saveit/src/main/java/br/com/saveit/apresentacao/UsuarioController.java
+++ Back-end/back-end/saveit/src/main/java/br/com/saveit/apresentacao/UsuarioController.java
@@ -1,10 +1,27 @@
 package br.com.saveit.apresentacao;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.saveit.dominio.Usuario;
+import br.com.saveit.dto.TelaCadastroUsuario;
+import br.com.saveit.dto.TelaLogin;
+import br.com.saveit.servico.ServicoUsuarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("Usuarios")
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    
+
+    @Autowired
+    private ServicoUsuarios servicoUsuarios;
+
+    @PostMapping("/cadastrar")
+    public Usuario cadastrar(@RequestBody TelaCadastroUsuario telaCadastro) {
+        return servicoUsuarios.cadastrar(telaCadastro);
+    }
+
+    @PostMapping("/login")
+    public Usuario login(@RequestBody TelaLogin telaLogin) {
+        return servicoUsuarios.autenticar(telaLogin);
+    }
 }
+
