d6fd5bb336157b8f2907a1cc61325204b159c32c
Myllena Navarro <128384896+Myllena-navarro@users.noreply.github.com>
2025-05-20 00:18:07 -0300

    Atualização tela inicial (cadastro/login)

--- saveitApp/src/screens/LoginCadastro.tsx
+++ saveitApp/src/screens/LoginCadastro.tsx
@@ -3,7 +3,6 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { styles } from '@/src/styles/styles';
 
-// Define o tipo de navegação para o nosso stack de rotas
 type RootStackParamList = {
   TelaInicial: undefined;
   LoginCadastro: undefined;
@@ -12,21 +11,17 @@ type RootStackParamList = {
 };
 
 export default function LoginCadastro() {
-  // Usando o hook useNavigation com o tipo de navegação
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   return (
     <View style={styles.container}>
-      {/* Logo no topo */}
       <Image
         source={require('@/assets/logo.jpeg')}
         style={styles.logo}
       />
 
-      {/* Título da tela */}
       <Text style={styles.title}>SaveIt</Text>
 
-      {/* Botão de Cadastro */}
       <TouchableOpacity
         style={[styles.button, styles.buttonMargin]}
         onPress={() => navigation.navigate('Cadastro')}
@@ -36,7 +31,6 @@ export default function LoginCadastro() {
         <Text style={styles.buttonText}>Cadastrar</Text>
       </TouchableOpacity>
 
-      {/* Botão de Login */}
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Login')}
