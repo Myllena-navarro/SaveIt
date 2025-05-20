b16e1adf79b9d3bce4aa1768001fd28e677ef69c
Myllena Navarro <128384896+Myllena-navarro@users.noreply.github.com>
2025-05-20 00:09:23 -0300

    Atualização confirmação despesas

--- saveitApp/src/screens/ConfirmacaoDespesa.tsx
+++ saveitApp/src/screens/ConfirmacaoDespesa.tsx
@@ -11,24 +11,20 @@ type RootStackParamList = {
 };
 
 export default function ConfirmacaoDespesa() {
-  // Instancia o hook de navegação com a tipagem correta
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   return (
     <View style={styles.containerCentered}>
-      {/* Ícone de sucesso - Check */}
       <Image
-        source={require('@/assets/check.jpeg')} // Imagem do ícone de sucesso
-        style={styles.iconLarge} // Estilo do ícone de sucesso
+        source={require('@/assets/check.jpeg')} 
+        style={styles.iconLarge} 
       />
 
-      {/* Título da tela */}
       <Text style={styles.title}>Despesa adicionada com sucesso!</Text>
 
-      {/* Botão de navegação para voltar ao início */}
       <TouchableOpacity
-        onPress={() => navigation.popToTop()} // Volta para o topo da pilha de navegação
-        style={[styles.button, { marginTop: 24 }]} // Estilo do botão, com margem adicional
+        onPress={() => navigation.popToTop()} 
+        style={[styles.button, { marginTop: 24 }]} 
       >
         <Text style={styles.buttonText}>Voltar ao Início</Text>
       </TouchableOpacity>
