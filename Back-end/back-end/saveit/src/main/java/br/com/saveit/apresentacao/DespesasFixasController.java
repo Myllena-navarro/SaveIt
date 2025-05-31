b6d050ed88b75297a76a53065f38f0a487056212
Debora Buriti <debora.buriti@liferay.com>
2025-05-31 16:54:41 -0300

    Alterar classes para garantir seguranca do projeto

--- Back-end/back-end/saveit/src/main/java/br/com/saveit/apresentacao/DespesasFixasController.java
+++ Back-end/back-end/saveit/src/main/java/br/com/saveit/apresentacao/DespesasFixasController.java
@@ -4,27 +4,50 @@ import br.com.saveit.dominio.Despesas;
 import br.com.saveit.dominio.Usuario;
 import br.com.saveit.dto.TelaDespesasFixas;
 import br.com.saveit.servico.ServicoDespesasFixas;
+import br.com.saveit.servico.ServicoUsuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/despesas-fixas")
 public class DespesasFixasController {
+
     @Autowired
     private ServicoDespesasFixas servicoDespesasFixas;
 
+    @Autowired
+    private ServicoUsuarios servicoUsuarios;
+
     @PostMapping("/cadastrar")
-    public ResponseEntity<Despesas> cadastrarDespesaFixa(
-            @RequestBody TelaDespesasFixas telaDespesasFixas,
-            @RequestAttribute("usuarioAutenticado") Usuario usuarioAutenticado) {
+    public ResponseEntity<Despesas> cadastrarDespesaFixa(@RequestBody TelaDespesasFixas telaDespesasFixas) {
         try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+
+            String emailUsuarioLogado = authentication.getName();
+
+            Usuario usuarioAutenticado = servicoUsuarios.buscarPorEmail(emailUsuarioLogado);
+
+            if (usuarioAutenticado == null) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
             Despesas despesaCadastrada = servicoDespesasFixas.cadastrarDespesaFixa(telaDespesasFixas, usuarioAutenticado);
             return new ResponseEntity<>(despesaCadastrada, HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
