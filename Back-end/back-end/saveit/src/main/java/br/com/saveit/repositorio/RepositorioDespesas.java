4595d73cf64f694dc6b95cb2c3a9a044c77bc2b9
Debora Buriti <debora.buriti@liferay.com>
2025-05-31 21:38:58 -0300

    Refatorar codigo

--- Back-end/back-end/saveit/src/main/java/br/com/saveit/repositorio/RepositorioDespesas.java
+++ Back-end/back-end/saveit/src/main/java/br/com/saveit/repositorio/RepositorioDespesas.java
@@ -2,11 +2,23 @@ package br.com.saveit.repositorio;
 
 import br.com.saveit.dominio.Categoria;
 import br.com.saveit.dominio.Despesas;
+import br.com.saveit.dto.RelatorioCategoriaDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
+import java.util.List;
+
 public interface RepositorioDespesas extends JpaRepository<Despesas, Long> {
 
+    @Query("SELECT new br.com.saveit.dto.RelatorioCategoriaDTO(d.categoria, SUM(d.valor)) " +
+           "FROM Despesas d " +
+           "WHERE (:categoria IS NULL OR d.categoria = :categoria) " +
+           "AND MONTH(d.dataCadastro) = :mes AND YEAR(d.dataCadastro) = :ano " +
+           "GROUP BY d.categoria")
+    List<RelatorioCategoriaDTO> obterRelatorioPorCategoria(
+        @Param("mes") int mes,
+        @Param("ano") int ano,
+        @Param("categoria") Categoria categoria
+    );
 }
-
