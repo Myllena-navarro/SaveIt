a6985cda9c024bd18dd3feec0c4fbdaf899a78a1
Debora Buriti <debora.buriti@liferay.com>
2025-05-31 21:42:55 -0300

    Adicionar DTO's para Relatorio financeiro

---
+++ Back-end/back-end/saveit/src/main/java/br/com/saveit/dto/FiltroRelatorioDTO.java
@@ -0,0 +1,41 @@
+package br.com.saveit.dto;
+
+public class FiltroRelatorioDTO {
+
+    private Integer mes;
+    private Integer ano;
+    private String categoria;
+    private String tipoDespesa; 
+
+    public Integer getMes() {
+        return mes;
+    }
+
+    public void setMes(Integer mes) {
+        this.mes = mes;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTipoDespesa() {
+        return tipoDespesa;
+    }
+
+    public void setTipoDespesa(String tipoDespesa) {
+        this.tipoDespesa = tipoDespesa;
+    }
+}
